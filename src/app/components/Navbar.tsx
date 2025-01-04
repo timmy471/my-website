315739a737e2feb5ab3564fa0266d32efe3bbd94
timmy471 <ayantunjitimilehin@gmail.com>
2025-01-04 15:18:24 +0100

    Add website content

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,10 +4,10 @@ import ThemeToggle from './ThemeToggle';
 
 const Navbar = () => {
   return (
-    <nav className='bg-white dark:bg-gray-800 shadow-md p-4'>
-      <div className='flex justify-between items-center'>
+    <nav className='py-4'>
+      <div className='container flex justify-between items-center'>
         <div className='flex items-center text-xl font-semibold text-gray-900 dark:text-white'>
-          <span>My Website</span>
+          <span>A.T</span>
         </div>
         <ThemeToggle />
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
-  return <div>works</div>;
-}
+import Image from 'next/image';
+
+const HomePage = () => {
+  return (
+    <div className='flex flex-col lg:flex-row gap-y-20 gap-x-4 container mt-20 items-center'>
+      <div className='lg:w-1/2'>
+        <Image
+          src='/images/ayantunjitimilehin.webp'
+          alt='Ayantunji Timilehin'
+          width={500}
+          height={500}
+          className='rounded-xl'
+        />
+      </div>
+
+      <div className='lg:w-1/2'>
+        <h4 className='text-2xl font-semibold'>Hi, I&apos;m</h4>
+        <h1 className='text-4xl font-bold text-gray-900 dark:text-white'>
+          Ayantunji Timilehin
+        </h1>
+        <ul className='mt-4 text-2xl space-y-2 list-disc text-gray-700 dark:text-gray-300'>
+          <li>Senior Software Engineer, Passionate About Performance</li>
+          <li>Technical Writer on FreeCodeCamp and Hackernoon</li>
+          <li>Open-source Contributor to Impactful Projects</li>
+          <li>Top African Mentor 2024 on ADPList</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
